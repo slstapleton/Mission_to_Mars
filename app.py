4b76b2eb3bb849d8f7aef830f59c467d7ef6d39a
slstapleton <staci.stapleton48@gmail.com>
2022-05-14 11:28:42 -0500

    mars

---
+++ app.py
@@ -0,0 +1,31 @@
+#use Flask to render a template, redirecting to another url, and creating a URL
+from flask import Flask, render_template, redirect, url_for
+#use PyMongo to interact with our Mongo database
+from flask_pymongo import PyMongo
+#to use the scraping code, we will convert from Jupyter notebook to Python
+import scraping
+
+#set up flask
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+#define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+#set up our scraping route
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update_one({}, {"$set":mars_data}, upsert=True)
+   return redirect('/', code=302)
+
+#tell flask to run
+if __name__ == "__main__":
+   app.run()
